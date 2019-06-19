a9a80dc0007a170c79b5772d401fad765b81a343
Arthur <arthur.siq5@gmail.com>
2019-06-19 20:45:51 -0300

    criada interface para salvar estado do museu

---
+++ src/src/dao/MuseuDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.dao;
+
+import src.negocio.Museu;
+
+/**
+ *
+ * @author arthur.siqueira
+ */
+public interface MuseuDao {
+    public void criaBase(String nomeDoMuseu);
+    public Museu buildMuseu(String nomeDoMuseu);
+    public boolean saveData(Museu museu);
+}
